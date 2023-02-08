85bcaf59b39379e52834750f24724bb47c43726f
Jonathan G <jonathan79410@gmail.com>
2023-02-09 00:15:20 +0100

    init scale=1

--- js/init.js
+++ js/init.js
@@ -121,16 +121,6 @@
 
 
 /*----------------------------------------------------*/
-/*	Dezoom pour mobile
-/*----------------------------------------------------*/
-
-var siteWidth = 900;
-var scale = screen.width /siteWidth;
-
-document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
-
-
-/*----------------------------------------------------*/
 /*	Flexslider
 /*----------------------------------------------------*/
    $('.flexslider').flexslider({
