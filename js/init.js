2abd0b0d6ecf0252fa9ffc42676593d71858b8a5
Jonathan G <jonathan79410@gmail.com>
2023-02-12 15:53:50 +0100

    gotop hide header/footer

--- js/init.js
+++ js/init.js
@@ -4,117 +4,124 @@
 /*
 -----------------------------------------------------------------------------------*/
 
- jQuery(document).ready(function($) {
-   
-  $('body').fadeIn(1000);
+jQuery(document).ready(function ($) {
 
-/*----------------------------------------------------*/
-/* Smooth Scrolling
------------------------------------------------------- */
+   $('body').fadeIn(1000);
 
-   $('.smoothscroll').on('click',function (e) {
-	    e.preventDefault();
+   /*----------------------------------------------------*/
+   /* Smooth Scrolling
+   ------------------------------------------------------ */
 
-	    var target = this.hash,
-	    $target = $(target);
+   $('.smoothscroll').on('click', function (e) {
+      e.preventDefault();
 
-	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
-	    }, 800, 'swing', function () {
-	        window.location.hash = target;
-	    });
-	});
+      var target = this.hash,
+         $target = $(target);
+
+      $('html, body').stop().animate({
+         'scrollTop': $target.offset().top
+      }, 800, 'swing', function () {
+         window.location.hash = target;
+      });
+   });
 
 
-/*----------------------------------------------------*/
-/* Highlight the current section in the navigation bar
-------------------------------------------------------*/
+   /*----------------------------------------------------*/
+   /* Highlight the current section in the navigation bar
+   ------------------------------------------------------*/
 
-	var sections = $("section");
-	var navigation_links = $("#nav-wrap a");
+   var sections = $("section");
+   var navigation_links = $("#nav-wrap a");
 
-	sections.waypoint({
+   sections.waypoint({
 
-      handler: function(event, direction) {
+      handler: function (event, direction) {
 
-		   var active_section;
+         var active_section;
 
-			active_section = $(this);
-			if (direction === "up") active_section = active_section.prev();
+         active_section = $(this);
+         if (direction === "up") active_section = active_section.prev();
 
-			var active_link = $('#nav-wrap a[href="#' + active_section.attr("id") + '"]');
+         var active_link = $('#nav-wrap a[href="#' + active_section.attr("id") + '"]');
 
          navigation_links.parent().removeClass("current");
-			active_link.parent().addClass("current");
+         active_link.parent().addClass("current");
 
-		},
-		offset: '25%'
+      },
+      offset: '25%'
 
-	});
-
-
-
-/*----------------------------------------------------*/
-/*	Make sure that #header-background-image height is
-/* equal to the browser height.
------------------------------------------------------- */
+   });
 
 
-$('header').css({ 'height': $(window).height() });
-$(window).on('resize', function() {  
-     $('header').css({ 'height': $(window).height() });
-});
+
+   /*----------------------------------------------------*/
+   /*	Make sure that #header-background-image height is
+   /* equal to the browser height.
+   ------------------------------------------------------ */
 
 
-/*----------------------------------------------------*/
-/*	Fade In/Out Primary Navigation
-------------------------------------------------------*/
+   $('header').css({ 'height': $(window).height() });
+   $(window).on('resize', function () {
+      $('header').css({ 'height': $(window).height() });
+   });
+
+
+   /*----------------------------------------------------*/
+   /*	Fade In/Out Primary Navigation
+   ------------------------------------------------------*/
    $(document).ready('DOMContentLoaded', manage_flag());
    $(window).on('scroll', manage_flag);
-   
-   function manage_flag()
-   {
-      
-		var h = $('header').height();
-		var y = $(window).scrollTop();
+
+   function manage_flag() {
+
+      var h = $('header').height();
+      var y = $(window).scrollTop();
       var nav = $('#nav-wrap');
       var flag = $('.flag');
       var gotop = $('#go-top-fixed');
 
       //Grand + sur mon nom
-	   if ( (y > h*0.18) && (y < h) && ($(window).outerWidth() > 900 )) {
-	      flag.fadeOut('fast');
-	      nav.fadeOut('fast');
-	      gotop.fadeOut('fast');
-	   }
+      if ((y > h * 0.18) && (y < h) && ($(window).outerWidth() > 900)) {
+         flag.fadeOut('fast');
+         nav.fadeOut('fast');
+      }
       //Petit + sur mon nom
-      else if ( (y > h*0.18) && (y < h) ) {
-	      flag.fadeOut('fast');
-	      gotop.fadeOut('fast');
-	   }
-      //en dessous de flèche gotop
-      else if (((document.querySelector('html').offsetHeight - document.querySelector('footer').offsetHeight - 60) - (window.innerHeight + window.scrollY)) < 0 ) {
-	      gotop.fadeOut('fast');
-	   }
+      else if ((y > h * 0.18) && (y < h)) {
+         flag.fadeOut('fast');
+      }
       else {
          //en haut
-         if (y < h*0.18) {
+         if (y < h * 0.18) {
             flag.fadeIn('fast');
             nav.removeClass('opaque').fadeIn('fast');
          }
          //tout le reste du site
          else {
             nav.addClass('opaque').fadeIn('fast');
-            gotop.fadeIn('fast');
             flag.hide();
          }
       }
 
-	}
 
-/*----------------------------------------------------*/
-/*	Flexslider
-/*----------------------------------------------------*/
+      //Manage top button
+      if (y < h)
+      {
+         gotop.fadeOut('fast');
+      }
+      else if (((document.querySelector('html').offsetHeight - document.querySelector('footer').offsetHeight - 60) - (window.innerHeight + window.scrollY)) < 0)
+      {
+         gotop.fadeOut('fast');
+      }
+      else {
+         gotop.fadeIn('fast');
+      }
+   }
+
+
+
+   /*----------------------------------------------------*/
+   /*	Flexslider
+   /*----------------------------------------------------*/
    $('.flexslider').flexslider({
       namespace: "flex-",
       controlsContainer: ".flex-container",
