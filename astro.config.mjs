d5081abe3da19e2006deaaa57860859d8ceb9808
Jonathan GATARD <jonathan.gatard@tichnou.fr>
2026-02-11 21:43:25 +0100

    chore(rename): update config for jonathan-g/cv repo

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jonathan-gatard.fr',
+  site: 'https://jonathan-gatard.fr', // Custom domain overrides github.io
+  // If not using custom domain: site: 'https://jonathan-g.github.io', 
   // base: '/cv',
   integrations: [tailwind()]
 });
